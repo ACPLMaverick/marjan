c74a8cc9facff1bc4da684b42c5ab0cd870ea11b
ACPLMaverick <MaverickCCCP@Gmail.com>
2014-10-12 23:46:20 +0200

    empty project on SW

---
+++ SW/workspace/mp3player/main/mp3player.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : mp3player.cpp
+// Author      : A10
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
