b7e55d582739f79c1734844bfdce14f327c7aa0c
ACPLMaverick <MaverickCCCP@Gmail.com>
2014-11-06 21:32:25 +0100

    car tracker update

--- PUM/workspace/CarTracker/src/com/plodz/cartracker/MapController.java
+++ PUM/workspace/CarTracker/src/com/plodz/cartracker/MapController.java
@@ -3,6 +3,11 @@ package com.plodz.cartracker;
 import java.io.Console;
 import java.util.ArrayList;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -14,15 +19,32 @@ import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 
 import android.app.FragmentManager;
+import android.content.IntentSender;
 import android.graphics.Color;
 import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
 
-public class MapController {
+public class MapController implements
+	GooglePlayServicesClient.ConnectionCallbacks,
+	GooglePlayServicesClient.OnConnectionFailedListener,
+	com.google.android.gms.location.LocationListener
+	
+{	
+	private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;	
+	private final static int MILISECONDS_PER_SECOND = 1000;
+	private final static int UPDATE_INTERVAL_IN_SECONDS = 10;
+	private final static long UPDATE_INTERVAL = MILISECONDS_PER_SECOND * UPDATE_INTERVAL_IN_SECONDS;
+	private final static int FASTEST_INTERVAL_IN_SECONDS = 5;
+	private final static long FASTEST_INTERVAL = MILISECONDS_PER_SECOND * FASTEST_INTERVAL_IN_SECONDS;
 	
 	public boolean isLoaded;
+	public boolean updatesRequested;
 	private boolean firstRun = true;
 	public GoogleMap map;
 	private TrackActivity activity;
+	private LocationClient myLocClient;
+	private LocationRequest myLocRequest;
 	private final float mapCameraZoom = 16.0f;
 	private final float polylineWidth = 25.0f;
 	private final int polylineColor = 0x7FFF0000;
@@ -34,42 +56,27 @@ public class MapController {
 	public MapController(TrackActivity activ)
 	{
 		isLoaded = false;
+		updatesRequested = false;
 		this.activity = activ;
 		FragmentManager manager = activity.getFragmentManager();
 		map = ((MyMapFragment)manager.findFragmentByTag("MAP")).getMap();
 		
+		myLocClient = new LocationClient(activity, this, this);
+		myLocRequest = LocationRequest.create();
+		myLocRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+		myLocRequest.setInterval(UPDATE_INTERVAL);
+		myLocRequest.setFastestInterval(FASTEST_INTERVAL);
+		
+		map.setMyLocationEnabled(true);
 		map.setOnMapLoadedCallback(new OnMapLoadedCallback()
 		{
 			@Override
 			public void onMapLoaded() {
-				// TODO Auto-generated method stub
 				isLoaded = true;
 			}
 			
 		}
 		);
-	}
-	
-	public void centerOnUser()
-	{
-		map.setMyLocationEnabled(true);
-		map.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener()
-				{
-
-					@Override
-					public void onMyLocationChange(Location arg0) {
-						LatLng location = new LatLng(arg0.getLatitude(), arg0.getLongitude());
-						map.animateCamera(CameraUpdateFactory.newLatLngZoom(location, mapCameraZoom));
-						if(!isLoaded)
-						{
-							isLoaded = true;
-							startLoc = arg0;
-							activity.startTracking();
-						}
-						else currentLoc = arg0;
-					}
-			
-				});
 	}
 	
 	public boolean createMarkerOnCurrentPos(float color, float alpha)
@@ -88,7 +95,7 @@ public class MapController {
 		map.addMarker(startMarkerOptions);
 		return true;
 	}
-	
+
 	public void initializePolyline()
 	{
 		PolylineOptions polyOpts = new PolylineOptions();
@@ -107,4 +114,61 @@ public class MapController {
 	}
 	
 	public Location getMyLocation() { return currentLoc; }
+	
+	public void connectLocationClient()
+	{
+		if(myLocClient != null) myLocClient.connect();
+	}
+	
+	public void disconnectLocationClient()
+	{
+		if(myLocClient != null) myLocClient.disconnect();
+		else System.out.println("kutas");
+	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult result) {
+		if(result.hasResolution())
+		{
+			try
+			{
+				result.startResolutionForResult(activity, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+			}
+			catch (IntentSender.SendIntentException e)
+			{
+				e.printStackTrace();
+			}
+		}
+		else
+		{
+			System.out.println("LOCCLIENT: Connection error " + String.valueOf(result.getErrorCode()));
+		}
+	}
+
+	@Override
+	public void onConnected(Bundle arg0) {
+		System.out.println("LOCCLIENT: Connected");
+		
+		updatesRequested = true;
+		myLocClient.requestLocationUpdates(myLocRequest, this);
+	}
+
+	@Override
+	public void onDisconnected() {
+		System.out.println("LOCCLIENT: Disconnected");
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+		LatLng loc = new LatLng(location.getLatitude(), location.getLongitude());
+		map.animateCamera(CameraUpdateFactory.newLatLngZoom(loc, mapCameraZoom));
+		
+		if(!isLoaded)
+		{
+			isLoaded = true;
+			startLoc = location;
+			activity.startTracking();
+		}
+		else currentLoc = location;
+	}
 }
--- PUM/workspace/CarTracker/src/com/plodz/cartracker/TrackActivity.java
+++ PUM/workspace/CarTracker/src/com/plodz/cartracker/TrackActivity.java
@@ -4,11 +4,12 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class TrackActivity extends Activity {
 	
 	TrackingController controller;
-	
+	Trip tripToSave;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -16,16 +17,39 @@ public class TrackActivity extends Activity {
 		setContentView(R.layout.activity_track);
 		
 		controller = new TrackingController(this);
+		controller.initialize();
+	}
+	
+	@Override
+	protected void onRestart()
+	{
+		super.onRestart();
+		// TODO: what happens when activity has focus again?
 	}
 	
 	@Override
 	protected void onStart()
 	{
 		super.onStart();
-		controller.initialize();
+		controller.connectLocalizationClient();
+	}
+	
+	@Override
+	protected void onStop()
+	{
+		super.onStop();
+	}
+	
+	@Override
+	protected void onDestroy()
+	{
+		super.onDestroy();
+		// TOOD: what happens when we quit tracking?
 	}
 	
 	public void startTracking() { controller.startTracking(); }
 	
-	public void endTrackingButtonClick() { controller.endTracking(); }
+	public void endTrackingButtonClick(View v) { controller.endTracking(); }
+	
+	public void pauseTrackingButtonClick(View v) { }
 }
