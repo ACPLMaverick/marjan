163c189cf47c5334ac8d3061ed2bd62b161f1754
ACPLMaverick <MaverickCCCP@Gmail.com>
2014-11-10 14:15:05 +0100

    jakies_pierdoly

--- PSG_Marcin/Engine2D/Engine2D/Text.h
+++ PSG_Marcin/Engine2D/Engine2D/Text.h
@@ -22,10 +22,27 @@ private:
 
 	Font* m_Font;
 	FontShader* m_FontShader;
+	int m_screenWidth, m_screenHeight;
+	D3DXMATRIX m_baseViewMatrix;
+
+	Sentence* sen01;
+	Sentence* sen02;
+
+	bool InitializeSentence(Sentence**, int, ID3D11Device*);
+	bool UpdateSentence(Sentence*, char*, int, int, float, float, float, ID3D11DeviceContext*);
+	void ReleaseSentence(Sentence**);
+	bool RenderSentence(ID3D11DeviceContext*, Sentence*, D3DXMATRIX, D3DXMATRIX);
 
 public:
+	char* fontCfgPath = "./Configs/FontBasic.cfg";
+	const string fontImgPath = "./Textures/FontBasic.cfg";
+
 	Text();
 	Text(const Text&);
 	~Text();
+
+	bool Initialize(ID3D11Device*, ID3D11DeviceContext*, HWND, int, int, D3DXMATRIX);
+	void Shutdown();
+	bool Render(ID3D11DeviceContext*, D3DXMATRIX, D3DXMATRIX);
 };
 
