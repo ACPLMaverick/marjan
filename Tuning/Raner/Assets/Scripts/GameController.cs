91b87d8ea97bc0b68b5dbfe7af799f56a86fadff
trolandus <johnnybe.beczcki@gmail.com>
2014-11-19 00:01:30 +0100

    Super zajebisty endless motherfuckin flyer/runner/shiter/crapper/star collecter

---
+++ Tuning/Raner/Assets/Scripts/GameController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	private float speed;
+	private float x;
+	private ArrayList backgrounds = new ArrayList();
+
+	public GameObject bg1, bg2, bg3;
+
+	// Use this for initialization
+	void Start () {
+		speed = 0.1f;
+		x = 3.0f;
+		backgrounds.Add (bg1);
+		backgrounds.Add (bg2);
+		backgrounds.Add (bg3);
+//		for(int i = 0; i<5; i++)
+//		{
+//			Instantiate(background, new Vector3(x,0.0f,0.0f), Quaternion.identity);
+//			x += 18.92864f;
+//		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Translate(this.transform.right * speed);
+		if(Time.timeSinceLevelLoad > x)
+		{
+			CreateBackground ();
+		}
+	}
+
+	void CreateBackground()
+	{
+		GameObject thisItem0 = (GameObject)backgrounds[0];
+		//GameObject thisItem1 = (GameObject)backgrounds[1];
+		//GameObject thisItem2 = (GameObject)backgrounds[2];
+		thisItem0.transform.Translate(57.422693f, 0.0f, 0.0f);
+		x += 2.45f;
+		//backgrounds [1] = backgrounds [0];
+		//backgrounds [2] = backgrounds [1];
+		//backgrounds [1] = thisItem0;
+		backgrounds.RemoveAt (0);
+		backgrounds.Insert (2, thisItem0);
+	}
+}
+
+//22.95219f, 0.007607499f
+//19.196271f
+// if(currentTime - lastTime > x) ?
+
---
+++ Tuning/Raner/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private float speed;
+
+	private float moveHorizontal;
+	private float moveVertical;
+	private Vector2 movement;
+	private Vector2 dashVector;
+
+	public GameObject star;
+
+	// Use this for initialization
+	void Start () {
+		speed = 25.0f;
+		dashVector = new Vector2(25000.0f, 0.0f);
+		star = GetComponent<PlayerController> ().star;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveHorizontal = Input.GetAxis ("Horizontal");
+		moveVertical = Input.GetAxis ("Vertical");
+		movement = new Vector2 (moveHorizontal, moveVertical);
+		this.rigidbody2D.velocity = movement * speed;
+
+		this.rigidbody2D.position = new Vector2(Mathf.Clamp(rigidbody2D.position.x, Camera.main.gameObject.transform.position.x - 8.0f, Camera.main.gameObject.transform.position.x + 8.0f), Mathf.Clamp (rigidbody2D.position.y, -3.5f, 3.5f));
+		if(Input.GetKeyUp(KeyCode.Space))
+		{
+			this.rigidbody2D.AddForceAtPosition(dashVector, rigidbody2D.position);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if(col.gameObject.tag == "Star")
+		{
+			Destroy(star);
+		}
+	}
+}
