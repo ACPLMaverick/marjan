ee2d0c2a721b69d4aed64e5a2d40b2152e043f80
trolandus <johnnybe.beczcki@gmail.com>
2014-11-19 22:32:49 +0100

    Poprawiłem generowanie levelu (hope so)

--- Tuning/Raner/Assets/Scripts/GameController.cs
+++ Tuning/Raner/Assets/Scripts/GameController.cs
@@ -12,21 +12,16 @@ public class GameController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		speed = 0.1f;
-		x = 3.0f;
+		x = 4.0f;
 		backgrounds.Add (bg1);
 		backgrounds.Add (bg2);
 		backgrounds.Add (bg3);
-//		for(int i = 0; i<5; i++)
-//		{
-//			Instantiate(background, new Vector3(x,0.0f,0.0f), Quaternion.identity);
-//			x += 18.92864f;
-//		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		this.transform.Translate(this.transform.right * speed);
-		if(Time.timeSinceLevelLoad > x)
+		if(Time.time > x)
 		{
 			CreateBackground ();
 		}
@@ -35,13 +30,8 @@ public class GameController : MonoBehaviour {
 	void CreateBackground()
 	{
 		GameObject thisItem0 = (GameObject)backgrounds[0];
-		//GameObject thisItem1 = (GameObject)backgrounds[1];
-		//GameObject thisItem2 = (GameObject)backgrounds[2];
 		thisItem0.transform.Translate(57.422693f, 0.0f, 0.0f);
-		x += 2.45f;
-		//backgrounds [1] = backgrounds [0];
-		//backgrounds [2] = backgrounds [1];
-		//backgrounds [1] = thisItem0;
+		x += 3.25f;
 		backgrounds.RemoveAt (0);
 		backgrounds.Insert (2, thisItem0);
 	}
