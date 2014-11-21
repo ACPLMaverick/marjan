df1d547e1a58512e37f14e8664a3bd2455a60f39
trolandus <johnnybe.beczcki@gmail.com>
2014-11-21 14:46:28 +0100

    Dodane nowe przeszkody, zbieranie gwiazdek działa lepiej

--- Tuning/Raner/Assets/Scripts/EnemySpawn.cs
+++ Tuning/Raner/Assets/Scripts/EnemySpawn.cs
@@ -3,20 +3,23 @@ using System.Collections;
 
 public class EnemySpawn : MonoBehaviour {
 
-    public GameObject enemy;
-    private Vector3 spawnPoint;
+    public GameObject enemy = null;
+    public GameObject obstacle = null;
+    //private Vector3 spawnPoint;
     private float spawnTime = 3.0f;
-    private float isSpawned;
     private bool isAlive;
     private float speed = 0.15f;
     private GameObject player;
     private GameObject clone;
+    private GameObject obstacleClone;
 
 	// Use this for initialization
 	void Start () {
         clone = GameObject.FindWithTag("Enemy");
+        obstacleClone = GameObject.FindWithTag("Obstacle");
         player = GameObject.FindWithTag("Player");
-        InvokeRepeating("Spawn", spawnTime, spawnTime);
+        if(enemy != null) InvokeRepeating("Spawn", spawnTime, spawnTime);
+        if(obstacle != null) InvokeRepeating("ObstacleSpawn", spawnTime, spawnTime);
 	}
 
     void Update()
@@ -27,12 +30,32 @@ public class EnemySpawn : MonoBehaviour {
     
     void Spawn()
     {
-        isSpawned = Random.Range(0.0f, 1.0f);
-        spawnPoint = new Vector3(player.transform.position.x + 20.0f, player.transform.position.y, -1.5f);
+        float isSpawned = Random.Range(0.0f, 1.0f);
+        Vector3 spawnPoint = new Vector3(player.transform.position.x + 20.0f, player.transform.position.y, -1.5f);
         if (isSpawned > 0.5f)
         {
             clone = (GameObject)Instantiate(enemy, spawnPoint, Quaternion.identity);
             Destroy(clone, 8.0f);
+        }
+    }
+
+    void ObstacleSpawn()
+    {
+        float isSpawned = Random.Range(0.0f, 1.0f);
+        Vector3 spawnPoint;
+        Quaternion spawnRotate;
+        if (isSpawned > 0.5f && isSpawned <= 0.75f)
+        {
+            spawnPoint = new Vector3(player.transform.position.x + 20.0f, -3.5f, -1.5f);
+            obstacleClone = (GameObject)Instantiate(obstacle, spawnPoint, Quaternion.identity);
+            Destroy(obstacleClone, 8.0f);
+        }
+        if(isSpawned > 0.75f)
+        {
+            spawnPoint = new Vector3(player.transform.position.x + 20.0f, 3.0f, -1.5f);
+            spawnRotate = Quaternion.Euler(0, 0, 180);
+            obstacleClone = (GameObject)Instantiate(obstacle, spawnPoint, spawnRotate);
+            Destroy(obstacleClone, 8.0f);
         }
     }
 }
